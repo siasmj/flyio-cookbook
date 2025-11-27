bf8358b60a5bc5beadd965e11c77c259f06b7da8
Matt Sias <mattsias@gmail.com>
2025-11-26 22:55:03 -0600

    Pacify Rubocop

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -12,7 +12,7 @@
 # Pagy.options[:client_max_limit] = 100   # The client can request a limit up to 100
 # Pagy.options[:max_pages] = 200          # Allow only 200 pages
 # Pagy.options[:jsonapi] = true           # Use JSON:API compliant URLs
-Pagy.options[:limit] = 20 
+Pagy.options[:limit] = 20
 
 ############ JavaScript ####################################################################
 # See https://ddnexus.github.io/pagy/resources/javascript/ for details.
